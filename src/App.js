885191d7f56c6a0cf175099a719bd15901aec645
Anthony Tam <anthonyy.tam@gmail.com>
2018-07-10 12:30:38 -0400

    testing react

--- src/App.js
+++ src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super ();
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="Lexis-fp">
+
+        <header className="Lexis-fp-header">
+          <h1 className="Lexis-fp-title">Lexis: </h1>
+          <p className="Lexis-fp-subtitle"> The Next Big Thing </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+        <p className="Lexis-fp-layer1">
+          Glen, lets put some content here.
+          <br></br><br></br>
+          Some cool ideas, you know?
         </p>
+
       </div>
     );
   }
 }
 
 export default App;
+
+
