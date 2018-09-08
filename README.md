9a0d9d3dd802b34ee3a792114833d9d16fca30f6
Anthony Tam <anthonyy.tam@gmail.com>
2018-09-08 14:41:17 -0400

    test backend api conn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Lexis Front Page
 <i>In development</i>
+
+Visit the [Project Page.](https://lexisco.github.io)
--- src/main/How.jsx
+++ src/main/How.jsx
@@ -3,22 +3,39 @@ import { Container, Header, Icon } from 'semantic-ui-react'
 
 
 class HowPage extends Component {
+
+// -- Backend Call Testing -- //
+  state = { response: ''};
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/start');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+// -- Backend Call Testing -- //
+
 	render() {
 		return(
 			<div className='hp-wrapper'>
 				<Container fluid textAlign='center' style={{ marginTop: '2em' }}>
 					<Header as='h2'>How it works</Header>
 					<p> <Icon name='wrench' /> </p>
-					<p>
-						It doesn't yet.
-					</p>
-					<p>
-						Will fill this up with technical jargon later.
-					</p>
-					<p>
-						<a href="https://github.com/LexisCo/lexisco.github.io">
-						View Github </a> 
-					</p>
+					<p> It doesn't yet. </p>
+					<p> Will fill this up with technical jargon later. </p>
+
+					{/* // -- Backend Call Testing -- // */}
+					<p> {this.state.response} </p>
+					{/* // -- Backend Call Testing -- // */}
+					
+					<p> <a href="https://github.com/LexisCo/lexisco.github.io"> 
+							View Github</a></p>
 				</Container>
 			</div>
 
